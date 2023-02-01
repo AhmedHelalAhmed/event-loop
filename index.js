e96060e2821ed93d64773bf10db7b6a61175404e
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-01 23:32:39 +0200

    case 5

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const fs = require('fs');
 const crypto = require("crypto");
 const start = Date.now();
-
 setTimeout(() => console.log("[Timer]: Timer 1 finished"), 0);//2
 setImmediate(() => console.log("[Immediate]: Immediate 1 finished"));//5
 
@@ -18,7 +17,17 @@ fs.readFile("test.txt", () => {
     process.nextTick(() => console.log("[nextTick]: NextTick 1 finished it executed between phases as it's microtask"));//4
 
     crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-        console.log("[crypto]: PBKDF2 finished with time: ",Date.now()-start);
+        console.log("[crypto]: PBKDF2 finished with time: ", Date.now() - start);
+    });
+
+    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+        console.log("[crypto]: PBKDF2 finished with time: ", Date.now() - start);
+    });
+    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+        console.log("[crypto]: PBKDF2 finished with time: ", Date.now() - start);
+    });
+    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+        console.log("[crypto]: PBKDF2 finished with time: ", Date.now() - start);
     });
 });
 
