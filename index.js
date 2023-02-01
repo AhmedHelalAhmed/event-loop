e473ccf3f579e3827dfa983a818973d8b3489bb6
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-01 23:18:57 +0200

    Add separator

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ setImmediate(() => console.log("[Immediate]: Immediate 1 finished"));//4
 
 fs.readFile("test.txt", () => {
     console.log("[I/O]: File read finished");//3
+    console.log("========================")
     setTimeout(() => console.log("[Timer]: Timer 2 finished"), 0);//6
     setTimeout(() => console.log("[Timer]: Timer 3 finished"), 3000);//7
     setImmediate(() => console.log("[Immediate]: Immediate 2 finished"));//5
